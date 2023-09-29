e124df1264b0d07d7de4b5e25972123c3d55b646
celclow <celclow@gmail.com>
2023-09-30 03:45:34 +0900

    Web用のhtml作成

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-console.log("Hello World!");
+import { AtokRomajiSettingGenerator } from "./atokRomajiSettingGenerator";
+
+document.getElementById("settings")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    try {
+        const sokuonKeys = (<HTMLInputElement>(
+            document.getElementById("sokuon-keys")
+        )).value;
+        const hatsuonKeys = (<HTMLInputElement>(
+            document.getElementById("hatsuon-keys")
+        )).value;
+        const romajiTable = (<HTMLTextAreaElement>(
+            document.getElementById("romaji-table")
+        )).value;
+        const outputFormat = Array.from(
+            <NodeListOf<HTMLInputElement>>(
+                document.getElementsByName("output-format")
+            ),
+        ).filter((node) => {
+            return node.checked;
+        })[0].value;
+
+        const generator = new AtokRomajiSettingGenerator();
+
+        for (const line of romajiTable.split("\n")) {
+            if (line.includes("\t")) {
+                const [romaji, kana] = line.split("\t");
+                generator.add(romaji, kana);
+            }
+        }
+
+        for (const c of sokuonKeys) {
+            generator.addSokuonCharacter(c);
+        }
+
+        for (const c of hatsuonKeys) {
+            generator.addHatsuonCharacter(c);
+        }
+
+        const result =
+            outputFormat === "windows"
+                ? generator.generateWindows().toString("hex")
+                : generator.generateMacos().toString("base64");
+
+        (<HTMLTextAreaElement>document.getElementById("result")).value = result;
+    } catch (e: any) {
+        (<HTMLTextAreaElement>document.getElementById("result")).value = e;
+    }
+});
